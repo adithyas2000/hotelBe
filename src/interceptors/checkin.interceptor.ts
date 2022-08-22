6e314b643badee8c96886beb5c08bee4c15327e5
adithyas2000 <adithyasaranga@gmail.com>
2022-08-22 05:58:51 +0530

    Added check-in function
    Check-in uses the same reservation model
    Created API, service and interceptor

---
+++ src/interceptors/checkin.interceptor.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import errorResponseHandler from "../../utils/errorResponseHandler";
+import { ErrorMessages } from "../../enums/enums";
+import Joi from "joi";
+
+export class CheckinValidators{
+    webCheckinValidator(req: Request, res: Response, next: NextFunction) {
+      const schema = Joi.object({
+        reservation_id: Joi.string().required(),
+        
+        arrival_date: Joi.string().required()
+      });
+      const errorState = schema.validate(req.body);
+  
+      if (errorState.error)
+        return next(errorResponseHandler(400, ErrorMessages.EMPTY_INPUT_FIELDS));
+  
+      next();
+    }
+  }
