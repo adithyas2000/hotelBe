88937efc155fb1d6f203006745e06cbed7de5f0c
adithyas2000 <adithyasaranga@gmail.com>
2022-08-24 06:45:26 +0530

    Creating customer doc, payment doc

--- src/service/checkin/checkin.service.ts
+++ src/service/checkin/checkin.service.ts
@@ -2,38 +2,18 @@ import { Request, Response, NextFunction } from "express";
 import { customAlphabet } from "nanoid";
 import LoggerGlobal from "../../../logger/loggerSingelton";
 import errorResponseHandler from "../../../utils/errorResponseHandler";
-
 import {
   ErrorMessages,
   ResponseStatus,
   ReservationStatus,
 } from "../../../enums/enums";
-
 import { Reservation } from "../../model/reservation/reservation";
 import { ReservationServices } from "../../service/reservation/reservation.service";
+import { Customer } from "../../model/customers/customer";
 const logger = LoggerGlobal.getInstance().logger;
 const reservationServices = new ReservationServices();
 export class CheckinServices {
-
-    async checkInCustomer(req: Request, res: Response, next: NextFunction) {
-        try {
-            const getcustomer = Reservation.findOne({ reservation_id: req.body.reservation_id }).lean().exec(function (err, resp) {
-                if (resp) {
-                    const updateCustomer = Reservation.findOneAndUpdate({ reservation_id: resp.reservation_id }, { arrival_date: req.body.arrival_date, status: ReservationStatus.CHECKED_IN }).lean().exec(function (err, update_res) {
-                        res.status(200).json({
-                            status: ResponseStatus.SUCCESS,
-                            data: {
-                                ...update_res
-                            },
-                        });
-                    });
-
-                } else {
-                    reservationServices.createANewReservation(req, res, next);
-                }
-
-
-  async checkInCustomerWithPriorReservation(
+  async checkInCustomerWithoutPriorReservation(
     req: Request,
     res: Response,
     next: NextFunction
@@ -43,24 +23,30 @@ export class CheckinServices {
         reservation_id: req.body.reservation_id,
       })
         .lean()
-        .exec(function (err, resp) {
+        .exec(async function (err, resp) {
           const updateCustomer = Reservation.findOneAndUpdate(
-            { reservation_id: resp.reservation_id },
+            { reservation_id: req.body.reservation_id },
             {
               arrival_date: req.body.arrival_date,
               status: ReservationStatus.CHECKED_IN,
             }
           )
             .lean()
-            .exec(function (err, update_res) {
-              res.status(200).json({
-                status: ResponseStatus.SUCCESS,
-                data: {
-                  ...update_res,
-                },
-              });
+            .exec(async function (err, update_res) {
 
+              const newCustomer=await Customer.create({
+                reservation_id:req.body.reservation_id,
+                room_number:req.body.room_number
+              });
+              
+              // res.status(200).json({
+              //   status: ResponseStatus.SUCCESS,
+              //   data: {
+              //     ...update_res,
+              //   },
+              // });
             });
+            
         });
       // create a customer record
 
@@ -72,5 +58,15 @@ export class CheckinServices {
         errorResponseHandler(500, ErrorMessages.INTERNAL_SERVER_ERROR)
       );
     }
+  }
+
+  async checkInCustomerWithPriorReservation(req:Request,res:Response,next:NextFunction){
+    this.checkInCustomerWithoutPriorReservation(req,res,next),
+    res.status(200).json({
+                status: ResponseStatus.SUCCESS,
+                data: {
+                  ...req.body,
+                },
+              });
   }
 }
